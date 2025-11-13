d452a5ad556b2c3af2118de0a4f792fb42b3216e
Aaron Celestian <aaron.celestian@gmail.com>
2025-11-13 12:12:25 -0800

    excess file deletion

--- __init__ (Aaron’s iMac 27-in 2020's conflicted copy 2025-11-11).py
+++
@@ -1,9 +0,0 @@
-"""
-RamanLab - Raman Spectroscopy Analysis Software
-
-This package provides tools for analyzing Raman spectroscopic data,
-including spectral processing, mineral identification, and database management.
-"""
-
-# Version
-__version__ = "1.3.3" 
